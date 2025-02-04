2ce165f33af03a62109a5cf47286b27b7926f958
esmith8860 <esmith8860@gmail.com>
2025-02-04 01:00:53 -0500

    C2

--- src/main/java/stepDefinitions/BuyingSwagStepDfn.java
+++ src/main/java/stepDefinitions/BuyingSwagStepDfn.java
@@ -37,7 +37,7 @@ public class BuyingSwagStepDfn {
     }
 
     @When("I enter an accepted username")
-    public void i_enter_an_accepted_username() throws InterruptedException {
+    public void i_enter_an_accepted_username() {
         driver.findElement(By.cssSelector("[name=\"user-name\"]")).sendKeys("standard_user");
     }
 
@@ -64,5 +64,60 @@ public class BuyingSwagStepDfn {
                 "Price (low to high)\n" +
                 "Price (high to low)");
         Thread.sleep(2000);
+    }
+
+    @Given("I successfully login")
+    public void i_successfully_login() throws InterruptedException {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("[value=\"Login\"]")).click();
+        Thread.sleep(1000);
+    }
+    @When("I click on Add to cart")
+    public void i_click_on_add_to_cart() throws InterruptedException {
+        driver.findElement(By.cssSelector("[id=\"add-to-cart-sauce-labs-backpack\"]")).click();
+        Thread.sleep(2000);
+    }
+    @And("I click on Your Cart")
+    public void i_click_on_your_cart() throws InterruptedException {
+        driver.findElement(By.cssSelector("[id=\"shopping_cart_container\"]")).click();
+        Thread.sleep(1000);
+    }
+    @And("I click on Checkout")
+    public void i_click_on_checkout() throws InterruptedException {
+        driver.findElement(By.cssSelector("[id=\"checkout\"]")).click();
+        Thread.sleep(2000);
+    }
+    @And("I enter a first name")
+    public void i_enter_a_first_name() {
+        driver.findElement(By.name("firstName")).sendKeys("John");
+    }
+    @And("I enter a last name")
+    public void i_enter_a_last_name() {
+        driver.findElement(By.name("lastName")).sendKeys("Doe");
+    }
+    @And("I enter a zip code")
+    public void i_enter_a_zip_code() throws InterruptedException {
+        driver.findElement(By.name("postalCode")).sendKeys("00000");
+        Thread.sleep(2000);
+    }
+    @And("I click Continue")
+    public void i_click_continue() throws InterruptedException {
+        driver.findElement(By.cssSelector("[id=\"continue\"]")).click();
+        Thread.sleep(2000);
+    }
+    @And("I click Finish")
+    public void i_click_finish() throws InterruptedException {
+        driver.findElement(By.cssSelector("[id=\"finish\"]")).click();
+        Thread.sleep(2000);
+    }
+    @Then("I should be presented with a Thank You message")
+    public void i_should_be_presented_with_a_thank_you_message() {
+        WebElement thankYouHeader = driver.findElement(By.xpath("//div[@id=\"checkout_complete_container\"]"));
+        Assert.assertEquals(thankYouHeader.getText(), "Thank you for your order!\n" +
+                "Your order has been dispatched, and will arrive just as fast as the pony can get there!\n" +
+                "Back Home");
     }
 }
--- src/test/resources/features/BuyingSwag.feature
+++ src/test/resources/features/BuyingSwag.feature
@@ -1,4 +1,4 @@
-Feature: Swag Labs - Login
+Feature: Swag Labs - Buying Swag
 
   Scenario: Validate Successful Login
     Given I am on the Swag Labs login page
@@ -6,3 +6,15 @@ Feature: Swag Labs - Login
     And I enter a matching password
     And I click login
     Then the inventory page should be displayed
+
+  Scenario: Validate Successful Purchase
+    Given I successfully login
+    When I click on Add to cart
+    And I click on Your Cart
+    And I click on Checkout
+    And I enter a first name
+    And I enter a last name
+    And I enter a zip code
+    And I click Continue
+    And I click Finish
+    Then I should be presented with a Thank You message
